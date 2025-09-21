e53478bbf477d54ed3db4af0912eda1c5b48b829
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2025-09-21 03:32:30 +0100

    commit

--- ipconnex_plaid/hooks.py
+++ ipconnex_plaid/hooks.py
@@ -11,13 +11,17 @@ app_email = "voip@ipconnex.com"
 app_license = "MIT"
 
 
-
-
 # Scheduled Tasks
 # ---------------
-
 scheduler_events = {
+    "hourly": [
+        "ipconnex_plaid.ipconnex_plaid.app.autoUpdatePlaid",
+    ]
 }
-
+doc_events = {
+    "Plaid Account": {
+        "after_insert": "ipconnex_plaid.ipconnex_plaid.app.loadTransactions"
+    },    
+}
 #app_install = "ipconnex_stripe_payment.ipconnex_stripe_payment.payement.setup_install"
 
--- ipconnex_plaid/ipconnex_plaid/app.py
+++ ipconnex_plaid/ipconnex_plaid/app.py
@@ -76,31 +76,28 @@ def getTransactions(client_id, client_secret, access_token, days, mode="sandbox"
 
     return all_transactions
 
-@frappe.whitelist() 
-def autoUpdatePlaid():
-    plaid_accounts=frappe.get_all("Plaid Account",fields=["name"],filters={"status": "Active"})
-
-    for pa in plaid_accounts : 
-        plaid_account=frappe.get_doc("Plaid Account",pa["name"])
-        transactions =getTransactions(
-                client_id=plaid_account.get_password("client_id"), 
-                client_secret=plaid_account.get_password("client_secret"), 
-                access_token=plaid_account.get_password("access_token"), 
-                days=plaid_account.refresh_days, 
-                mode=plaid_account.account_type)
-        for transaction in transactions :
+def loadTransactions(doc):
+    days_count = (date.today() - date(2020, 1, 1)).days
+    plaid_account=frappe.get_doc("Plaid Account",doc.name)
+    transactions =getTransactions(
+            client_id=plaid_account.get_password("client_id"), 
+            client_secret=plaid_account.get_password("client_secret"), 
+            access_token=plaid_account.get_password("access_token"), 
+            days=days_count, 
+            mode=plaid_account.account_type)
+    for transaction in transactions :
+        try:
             t_date=date.today()
             if transaction["date"]:
                 t_date=transaction["date"]
             t_confidence = "UNKNOWN"
             t_detailed = ""
             t_icon=""
-
             if transaction["personal_finance_category"]:
                 if transaction["personal_finance_category"]["confidence_level"]:
                     t_confidence=transaction["personal_finance_category"]["confidence_level"]
                 if transaction["personal_finance_category"]["detailed"]:
-                   t_detailed=transaction["personal_finance_category"]["detailed"]
+                    t_detailed=transaction["personal_finance_category"]["detailed"]
             if len(transaction["counterparties"]):
                 if transaction["counterparties"][0]["logo_url"]:
                     t_icon=transaction["counterparties"][0]["logo_url"]
@@ -128,10 +125,66 @@ def autoUpdatePlaid():
                 data["doctype"]="Plaid Transaction"
                 doc = frappe.get_doc(data)
                 doc.insert()
+        except :
+            pass
 
 
 
 
-
-
+def autoUpdatePlaid():
+    plaid_accounts=frappe.get_all("Plaid Account",fields=["name"],filters={"status": "Active"})
+    for pa in plaid_accounts : 
+        try :
+            plaid_account=frappe.get_doc("Plaid Account",pa["name"])
+            transactions =getTransactions(
+                    client_id=plaid_account.get_password("client_id"), 
+                    client_secret=plaid_account.get_password("client_secret"), 
+                    access_token=plaid_account.get_password("access_token"), 
+                    days=plaid_account.refresh_days, 
+                    mode=plaid_account.account_type)
+            for transaction in transactions :
+                
+                try :
+                    t_date=date.today()
+                    if transaction["date"]:
+                        t_date=transaction["date"]
+                    t_confidence = "UNKNOWN"
+                    t_detailed = ""
+                    t_icon=""
+                    if transaction["personal_finance_category"]:
+                        if transaction["personal_finance_category"]["confidence_level"]:
+                            t_confidence=transaction["personal_finance_category"]["confidence_level"]
+                        if transaction["personal_finance_category"]["detailed"]:
+                            t_detailed=transaction["personal_finance_category"]["detailed"]
+                    if len(transaction["counterparties"]):
+                        if transaction["counterparties"][0]["logo_url"]:
+                            t_icon=transaction["counterparties"][0]["logo_url"]
+                    data={
+                        "account":pa["name"],
+                        "account_id":transaction["account_id"],
+                        "transaction_id":transaction["transaction_id"],
+                        "transaction_type":"Debit" if  transaction["amount"] < 0 else "Credit",
+                        "transaction_icon":t_icon,
+                        "category":",".join(transaction["category"]),
+                        "amount":transaction["amount"],
+                        "currency":transaction["iso_currency_code"],
+                        "date":t_date.strftime("%Y-%m-%d") , 
+                        "merchant_entity_id":transaction["merchant_entity_id"],
+                        "merchant_name":transaction["merchant_name"],
+                        "confidence_level":t_confidence,
+                        "detailed":t_detailed, 
+                        "pending":transaction["pending"],
+                        "website":transaction["website"]
+                    }
+                    if frappe.db.exists("Plaid Transaction", transaction["transaction_id"]):
+                        for field, value in data.items():
+                            frappe.db.set_value("Plaid Transaction", transaction["transaction_id"], field, value)
+                    else:
+                        data["doctype"]="Plaid Transaction"
+                        doc = frappe.get_doc(data)
+                        doc.insert()        
+                except :
+                    pass
+        except :
+            pass
 
