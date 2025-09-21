d105194fb678fdea453c1e9489a10d63404818b6
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2025-09-21 20:26:01 +0100

    c

--- ipconnex_plaid/ipconnex_plaid/doctype/plaid_account/plaid_account.js
+++ ipconnex_plaid/ipconnex_plaid/doctype/plaid_account/plaid_account.js
@@ -5,14 +5,14 @@ document.head.appendChild(script);
 frappe.ui.form.on("Plaid Account", {
   refresh: function (frm) {
     frm.add_custom_button(__("Update"), function (event) {
-      $('button[data-label="Pay%20Invoice"]').prop("disabled", true);
+      $('button[data-label="Update"]').prop("disabled", true);
       frappe.call({
         method: "ipconnex_plaid.ipconnex_plaid.app.updatePlaidAccount",
         args: {
           doc_name: frm.doc.name,
         },
         callback: function (res) {
-          $('button[data-label="Pay%20Invoice"]').prop("disabled", false);
+          $('button[data-label="Update"]').prop("disabled", false);
           if (res.message.status == 1) {
             frm.reload_doc();
             Swal.fire({
