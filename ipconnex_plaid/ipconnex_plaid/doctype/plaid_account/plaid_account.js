6896cf78e64878a22d39b91c92934f3907bc2d6b
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2025-09-21 20:15:07 +0100

    ci

--- ipconnex_plaid/ipconnex_plaid/doctype/plaid_account/plaid_account.js
+++ ipconnex_plaid/ipconnex_plaid/doctype/plaid_account/plaid_account.js
@@ -4,31 +4,31 @@ document.head.appendChild(script);
 
 frappe.ui.form.on("Plaid Account", {
   refresh: function (frm) {
-    $("button[data-fieldname='Update']")
-      .off("click")
-      .on("click", function () {
-        frappe.call({
-          method: "ipconnex_plaid.ipconnex_plaid.app.updatePlaidAccount",
-          args: {
-            doc_name: frm.doc.name,
-          },
-          callback: function (res) {
-            if (res.message.status == 1) {
-              frm.reload_doc();
-              Swal.fire({
-                icon: "success",
-                title: "Success",
-                text: res.message.message,
-              });
-            } else {
-              Swal.fire({
-                icon: "warning",
-                title: "Warning",
-                text: res.message.message,
-              });
-            }
-          },
-        });
+    frm.add_custom_button(__("Pay Invoice"), function (event) {
+      $('button[data-label="Pay%20Invoice"]').prop("disabled", true);
+      frappe.call({
+        method: "ipconnex_plaid.ipconnex_plaid.app.updatePlaidAccount",
+        args: {
+          doc_name: frm.doc.name,
+        },
+        callback: function (res) {
+          $('button[data-label="Pay%20Invoice"]').prop("disabled", false);
+          if (res.message.status == 1) {
+            frm.reload_doc();
+            Swal.fire({
+              icon: "success",
+              title: "Success",
+              text: res.message.message,
+            });
+          } else {
+            Swal.fire({
+              icon: "warning",
+              title: "Warning",
+              text: res.message.message,
+            });
+          }
+        },
       });
+    });
   },
 });
